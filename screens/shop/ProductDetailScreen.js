377b204f227486c5cf4f3694be7a5c608d1ce575
Elgur <leroy.maxence@yahoo.com>
2020-03-23 13:11:13 +0100

    added cartIcon to the header and cart section

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -7,8 +7,10 @@ import {
 	Button,
 	ScrollView
 } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
 import Colors from "../../constants/Colors";
+import * as cartActions from "../../store/actions/cart";
 
 const ProductDetailScreen = props => {
 	const productId = props.navigation.getParam("productId");
@@ -17,6 +19,8 @@ const ProductDetailScreen = props => {
 			product => product.id === productId
 		)
 	);
+
+	const dispatch = useDispatch();
 
 	return (
 		<ScrollView>
@@ -28,7 +32,9 @@ const ProductDetailScreen = props => {
 				<Button
 					color={Colors.primary}
 					title="Add to Cart"
-					onPress={() => {}}
+					onPress={() => {
+						dispatch(cartActions.addToCart(selectedProduct));
+					}}
 				/>
 			</View>
 			<Text style={styles.price}>
@@ -53,22 +59,22 @@ const styles = StyleSheet.create({
 	image: {
 		width: "100%",
 		height: 300
-    },
-    actions: {
-        marginVertical: 10,
-        alignItems: "center"
-    },
+	},
+	actions: {
+		marginVertical: 10,
+		alignItems: "center"
+	},
 	price: {
 		fontSize: 20,
 		color: "#888",
 		textAlign: "center",
-        marginVertical: 20,
-        fontFamily: "open-sans-bold"
+		marginVertical: 20,
+		fontFamily: "open-sans-bold"
 	},
 	description: {
-        fontFamily: "open-sans",
+		fontFamily: "open-sans",
 		fontSize: 14,
-        textAlign: "center",
-        marginHorizontal: 20
+		textAlign: "center",
+		marginHorizontal: 20
 	}
 });
