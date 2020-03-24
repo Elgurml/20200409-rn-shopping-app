33d50a0cb9854f2eeb56b049f3bf71da6c7f5b3f
Elgur <leroy.maxence@yahoo.com>
2020-03-24 12:47:51 +0100

    side drawer done and order screen added

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
 import * as cartActions from "../../store/actions/cart";
+import * as ordersActions from "../../store/actions/orders";
 
 const CartScreen = props => {
 	const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -39,6 +40,11 @@ const CartScreen = props => {
 					color={Colors.accent}
 					title="Order Now"
 					disabled={cartItems.length === 0}
+					onPress={() => {
+						dispatch(
+							ordersActions.addOrder(cartItems, cartTotalAmount)
+						);
+					}}
 				/>
 			</View>
 			<FlatList
@@ -61,6 +67,10 @@ const CartScreen = props => {
 			/>
 		</View>
 	);
+};
+
+CartScreen.navigationOptions = {
+	headerTitle: "Your Cart"
 };
 
 export default CartScreen;
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { FlatList, Platform } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
-
-import ProductItem from "../../components/shop/ProductItem";
-import * as cartActions from "../../store/actions/cart";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
+import ProductItem from "../../components/shop/ProductItem";
+import * as cartActions from "../../store/actions/cart";
 
 const ProductsOverviewScreen = props => {
 	const products = useSelector(state => state.products.availableProducts);
@@ -37,6 +36,19 @@ const ProductsOverviewScreen = props => {
 ProductsOverviewScreen.navigationOptions = navData => {
 	return {
 		headerTitle: "All Products",
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title="Menu"
+					iconName={
+						Platform.OS === "android" ? "md-menu" : "ios-menu"
+					}
+					onPress={() => {
+						navData.navigation.toggleDrawer();
+					}}
+				/>
+			</HeaderButtons>
+		),
 		headerRight: () => (
 			<HeaderButtons HeaderButtonComponent={HeaderButton}>
 				<Item
