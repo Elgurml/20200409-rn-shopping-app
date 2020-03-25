feb917e2f3161198084fe72b0ef259be8366092b
Elgur <leroy.maxence@yahoo.com>
2020-03-25 17:01:01 +0100

    show/hide details button now displays the items in order

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -17,7 +17,7 @@ const CartItem = props => {
 			</View>
 			<View style={styles.itemData}>
 				<Text style={styles.mainText}>{props.amount.toFixed(2)}</Text>
-				<TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
+				{props.deletable && <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
 					<Ionicons
 						name={
 							Platform.OS === "android" ? "md-trash" : "ios-trash"
@@ -25,7 +25,7 @@ const CartItem = props => {
 						size={23}
 						color="red"
 					/>
-				</TouchableOpacity>
+				</TouchableOpacity>}
 			</View>
 		</View>
 	);
--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import CartItem from "./CartItem";
 import Colors from "../../constants/Colors";
 
 const OrderItem = props => {
+	const [showDetails, setShowDetails] = useState(false);
+
 	return (
 		<View style={styles.orderItem}>
 			<View style={styles.summary}>
@@ -12,7 +14,25 @@ const OrderItem = props => {
 				</Text>
 				<Text style={styles.date}>{props.date}</Text>
 			</View>
-			<Button color={Colors.primary} title="Show Details " />
+			<Button
+				color={Colors.primary}
+				title={showDetails ? "Hide Details" : "Show Details "}
+				onPress={() => {
+					setShowDetails(prevState => !prevState);
+				}}
+			/>
+			{showDetails && (
+				<View style={styles.detailItems}>
+					{props.items.map(cartItem => (
+						<CartItem
+                            key={cartItem.productId}
+							quantity={cartItem.quantity}
+							amount={cartItem.sum}
+							title={cartItem.productTitle}
+						/>
+					))}
+				</View>
+			)}
 		</View>
 	);
 };
@@ -20,32 +40,35 @@ const OrderItem = props => {
 export default OrderItem;
 
 const styles = StyleSheet.create({
-    orderItem: {
-        shadowColor: "black",
+	orderItem: {
+		shadowColor: "black",
 		shadowOpacity: 0.6,
 		shadowOffset: { width: 0, height: 20 },
 		shadowRadius: 8,
 		elevation: 5,
 		borderRadius: 10,
-        backgroundColor: "white",
-        margin: 20,
-        padding: 10,
-        alignItems: "center"
+		backgroundColor: "white",
+		margin: 20,
+		padding: 10,
+		alignItems: "center"
+	},
+	summary: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		width: "100%",
+		marginBottom: 15
+	},
+	totalAmount: {
+		fontFamily: "open-sans-bold",
+		fontSize: 16
+	},
+	date: {
+		fontFamily: "open-sans",
+		fontSize: 16,
+		color: "#888"
     },
-    summary: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-        marginBottom: 15
-    },
-    totalAmount: {
-        fontFamily: "open-sans-bold",
-        fontSize: 16
-    },
-    date: {
-        fontFamily: "open-sans",
-        fontSize: 16,
-        color: "#888"
+    detailItems: {
+        width: "100%"
     }
 });
