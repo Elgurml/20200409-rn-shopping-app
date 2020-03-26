3dd6fe828b50e61dd48523e0fe28140ed971cc0c
Elgur <leroy.maxence@yahoo.com>
2020-03-26 12:35:48 +0100

    can now delete a product in the admin section

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { StyleSheet, FlatList, Platform, Button } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
 import ProductItem from "../../components/shop/ProductItem";
+import * as productsActions from "../../store/actions/products"
 import Colors from "../../constants/Colors"
 
 const UserProductsScreen = () => {
 	const userProducts = useSelector(state => state.products.userProducts);
+	const dispatch = useDispatch()
 	return (
 		<FlatList
 			data={userProducts}
@@ -33,7 +35,7 @@ const UserProductsScreen = () => {
 						color={Colors.primary}
 						title="Delete"
 						onPress={() => {
-							dispatch(cartActions.addToCart(itemData.item));
+							dispatch(productsActions.deleteProduct(itemData.item.id));
 						}}
 					/>
 				</ProductItem>
