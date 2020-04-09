b6bf7b26a98a619ad93ea55aa7a76c9713c88d71
Elgur <leroy.maxence@yahoo.com>
2020-04-09 11:26:05 +0200

    auto login when token and userId is valid

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -5,78 +5,75 @@ import { createDrawerNavigator } from "react-navigation-drawer";
 import { Platform } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-import AuthScreen from "../screens/user/AuthScreen"
+import AuthScreen from "../screens/user/AuthScreen";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
-import UserProductsScreen from "../screens/user/UserProductsScreen"
-import EditProductScreen from "../screens/user/EditProductScreen"
+import UserProductsScreen from "../screens/user/UserProductsScreen";
+import EditProductScreen from "../screens/user/EditProductScreen";
+import StartUpScreen from "../screens/StartUpScreen";
 import Colors from "../constants/Colors";
 
 const defaultNavOptions = {
 	headerStyle: {
-		backgroundColor: Platform.OS === "android" ? Colors.primary : ""
+		backgroundColor: Platform.OS === "android" ? Colors.primary : "",
 	},
 	headerTitleStyle: {
-		fontFamily: "open-sans-bold"
+		fontFamily: "open-sans-bold",
 	},
 	headerBackTitleStyle: {
-		fontFamily: "open-sans"
+		fontFamily: "open-sans",
 	},
-	headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
+	headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
 };
 
 const ProductsNavigator = createStackNavigator(
 	{
 		ProductsOverview: ProductsOverviewScreen,
 		ProductDetail: ProductDetailScreen,
-		Cart: CartScreen
+		Cart: CartScreen,
 	},
 	{
 		navigationOptions: {
-			drawerIcon: drawerConfig => (
+			drawerIcon: (drawerConfig) => (
 				<Ionicons
-					name={
-						Platform.OS === "android" ? "md-cart" : "ios-cart"
-					}
+					name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
 					size={23}
 					color={drawerConfig.tintColor}
 				/>
-			)
+			),
 		},
-		defaultNavigationOptions: defaultNavOptions
+		defaultNavigationOptions: defaultNavOptions,
 	}
 );
 
 const OrdersNavigator = createStackNavigator(
 	{
-		Orders: OrdersScreen
+		Orders: OrdersScreen,
 	},
 	{
 		navigationOptions: {
-			drawerIcon: drawerConfig => (
+			drawerIcon: (drawerConfig) => (
 				<Ionicons
-					name={
-						Platform.OS === "android" ? "md-list" : "ios-list"
-					}
+					name={Platform.OS === "android" ? "md-list" : "ios-list"}
 					size={23}
 					color={drawerConfig.tintColor}
 				/>
-			)
+			),
 		},
-		defaultNavigationOptions: defaultNavOptions
+		defaultNavigationOptions: defaultNavOptions,
 	}
 );
 
 const AdminNavigator = createStackNavigator(
 	{
 		UserProducts: UserProductsScreen,
-		EditProduct: EditProductScreen
+		EditProduct: EditProductScreen,
 	},
 	{
 		navigationOptions: {
-			drawerIcon: drawerConfig => (
+			drawerIcon: (drawerConfig) => (
 				<Ionicons
 					name={
 						Platform.OS === "android" ? "md-create" : "ios-create"
@@ -84,9 +81,9 @@ const AdminNavigator = createStackNavigator(
 					size={23}
 					color={drawerConfig.tintColor}
 				/>
-			)
+			),
 		},
-		defaultNavigationOptions: defaultNavOptions
+		defaultNavigationOptions: defaultNavOptions,
 	}
 );
 
@@ -94,25 +91,28 @@ const ShopNavigator = createDrawerNavigator(
 	{
 		Products: ProductsNavigator,
 		Orders: OrdersNavigator,
-		Admin: AdminNavigator
+		Admin: AdminNavigator,
 	},
 	{
 		contentOptions: {
-			activeTintColor: Colors.primary
-		}
+			activeTintColor: Colors.primary,
+		},
 	}
 );
 
-const AuthNavigator = createStackNavigator({
-	Auth: AuthScreen
-},
-{
-	defaultNavigationOptions: defaultNavOptions
-})
+const AuthNavigator = createStackNavigator(
+	{
+		Auth: AuthScreen,
+	},
+	{
+		defaultNavigationOptions: defaultNavOptions,
+	}
+);
 
 const MainNavigator = createSwitchNavigator({
+	Startup: StartUpScreen,
 	Auth: AuthNavigator,
-	Shop: ShopNavigator
-})
+	Shop: ShopNavigator,
+});
 
 export default createAppContainer(MainNavigator);
--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,5 +1,13 @@
-export const SIGNUP = "SIGNUP";
-export const LOGIN = "LOGIN";
+// AsyncStorage is an API that uses a key value storage on the device with data that persists on app relaunch
+import { AsyncStorage } from "react-native";
+
+// export const SIGNUP = "SIGNUP";
+// export const LOGIN = "LOGIN";
+export const AUTHENTICATE = "AUTHENTICATE"
+
+export const authenticate = (userId, token) => {
+	return { type: AUTHENTICATE, userId: userId, token: token }
+}
 
 export const signup = (email, password) => {
 	return async (dispatch) => {
@@ -25,12 +33,17 @@ export const signup = (email, password) => {
 			if (errorId === "EMAIL_EXISTS") {
 				message = "This email already exists!";
 			}
-			throw new Error(message)
+			throw new Error(message);
 		}
 
 		const resData = await response.json();
 		console.log("signup--------", resData);
-		dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
+		dispatch(authenticate(resData.localId, resData.idToken));
+		// getTime in ms and resData.expiresIn in sec so => * 1000
+		const expirationDate = new Date(
+			new Date().getTime() + parseInt(resData.expiresIn) * 1000
+		);
+		saveDataToStorage(resData.idToken, resData.localId);
 	};
 };
 
@@ -58,13 +71,29 @@ export const login = (email, password) => {
 			if (errorId === "EMAIL_NOT_FOUND") {
 				message = "This email could not be found!";
 			} else if (errorId === "INVALID_PASSWORD") {
-				message = "This password is not valid!"
+				message = "This password is not valid!";
 			}
-			throw new Error(message)
+			throw new Error(message);
 		}
 
 		const resData = await response.json();
 		console.log("login--------", resData);
-		dispatch({ type: LOGIN, token: resData.idToken, userId: resData.localId });
+		dispatch(authenticate(resData.localId, resData.idToken));
+		// getTime in ms and resData.expiresIn in sec so => * 1000
+		const expirationDate = new Date(
+			new Date().getTime() + parseInt(resData.expiresIn) * 1000
+		);
+		saveDataToStorage(resData.idToken, resData.localId, expirationDate);
 	};
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+	AsyncStorage.setItem(
+		"userData",
+		JSON.stringify({
+			token: token,
+			userId: userId,
+			expiryDate: expirationDate.toISOString(),
+		})
+	);
 };
